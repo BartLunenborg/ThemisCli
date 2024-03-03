963a7f8904317cc97ee14f87bc67ec6e0e2e1c2f
Bart Lunenborg <b.lunenborg@student.rug.nl>
2024-03-03 17:50:30 +0100

    Cleaner code

--- bin/setYear.py
+++ bin/setYear.py
@@ -1,46 +1,41 @@
 import os
 
+OPTIONS = [
+    "2021-2022",
+    "2022-2023",
+    "2023-2024",
+    "No year",
+    "Quit",
+]
+
 
 def get_year():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     yearFile = os.path.join(script_dir, "year.txt")
     if os.path.exists(yearFile):
         with open(yearFile, "r") as file:
-            stored_year = file.read().strip()
-            if stored_year:
+            if stored_year := file.read().strip():
                 print(f"Current year set to: {stored_year}")
 
     print("Pick a year to set Themis to:")
-    print("(1) 2021-2022")
-    print("(2) 2022-2023")
-    print("(3) 2023-2024")
-    print("(4) No year")
-    print("(5) Quit")
+    for i, option in enumerate(OPTIONS):
+        print(f"({i+1}) {option}")
 
     choice = input("Enter the number of your choice: ")
-    if choice == "1":
-        selected_year = "2021-2022"
-    elif choice == "2":
-        selected_year = "2022-2023"
-    elif choice == "3":
-        selected_year = "2023-2024"
+
+    if "1" <= choice <= "3":
+        selected_year = OPTIONS[int(choice) - 1]
+        with open(yearFile, "w") as file:
+            file.write(selected_year)
+        print(f"Year set to: {selected_year}")
     elif choice == "4":
+        print("Year is no longer set.")
         if os.path.exists(yearFile):
             os.remove(yearFile)
-        exit()
-    elif choice == "5":
-        print("Exiting...")
-        exit()
-    else:
+    elif choice != "5":
         print("Invalid choice. Please enter a valid option.")
-        print("Exiting")
-        exit()
-
-    with open(yearFile, "w") as file:
-        file.write(selected_year)
-
-    print(f"Year set to: {selected_year}")
-    return selected_year
+    
+    print("Exiting")
 
 
 if __name__ == "__main__":
